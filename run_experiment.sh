4a94a4811750159b1304d121ce0eae630f847708
vepricov <veprikov.as@phystech.edu>
2023-04-27 22:17:40 +0300

    finalazing

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,13 +1,13 @@
 trap 'exit' SIGINT SIGTERM SIGHUP SIGQUIT
 
 # for usage in 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95
-# for adherence in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+# for adherence in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5
 
 for step in 10
 do
-    for usage in 1.0
+    for usage in 1
     do
-        for adherence in 0.0
+        for adherence in 0
         do
           export step &&
           export usage &&
